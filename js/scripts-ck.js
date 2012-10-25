228992f404a940d44e5ff5beb700a439cfaf39f4
Terence Devine <info@terencedevine.com>
2012-10-25 13:32:48 -0600

    update scripts files - add media queries

---
+++ js/scripts-ck.js
@@ -0,0 +1,2 @@
+// Basic Function
+(function(e){})(jQuery);enquire.register("screen and (max-width:1000px)",{match:function(){},unmatch:function(){},setup:function(){},destroy:function(){}}).listen();
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Basic Function
+(function($) {
+	// write some awesome functions!
+})(jQuery);
+
+// Functions using Enquire.js
+enquire
+	.register("screen and (max-width:1000px)", {
+
+    match : function() {
+
+    },
+    unmatch : function() {
+
+    },
+    setup : function() {
+
+    },
+    destroy : function() {
+
+    }
+}).listen();
